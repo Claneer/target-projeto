fbd1c875db0083c63573452c7ceb925cedc4856c
Claneer <pedrovickluke5@gmail.com>
2024-09-03 18:26:40 -0300

    add via commit

--- target.py
+++ target.py
@@ -1,3 +1,4 @@
+import json
 def Questao1():
     indice=13
     soma=0
@@ -26,12 +27,13 @@ def Questao2(a):
         print(f"o número {z} não pertence a sequencia de fibonacci")
     
 def Questao3():
-    fatu = [1660, 2000, 1500, 4605, 2077, 0, 0, 4056, 3500, 0, 0, 5743, 4733, 0, 0, 6503, 5275, 0, 0, 7624, 6500, 0, 0, 8000, 7840, 0, 0, 9000, 10000, 0, 0]
-    fatu = [f for f in fatu if f > 0]
-    x = min(fatu)
-    y = max(fatu)
-    mm = sum(fatu) / len(fatu)
-    z = sum(1 for f in fatu if f > mm)
+    with open("dados.json","r")as file:
+        dados = json.load(file)
+    valores = [item["valor"] for item in dados if item["valor"] > 0]
+    x = min(valores)
+    y = max(valores)
+    mm = sum(valores) / len(valores)
+    z = sum(1 for valor in valores if valor > mm)
 
     print("O menor faturamento diario foi:",x)
     print("O maior faturamento diario foi:",y)
@@ -46,8 +48,8 @@ def Questao4():
     print(f"Em ordem(SP,RJ,MG,ES,OUTROS){y}%")
 
 def Questao5():
-   x=str(input("Escreva qualquer palavra"))
+   x=str(input("Escreva qualquer palavra\n"))
    y=x[::-1]
    print(y)
 
-Questao5()
+Questao3()
